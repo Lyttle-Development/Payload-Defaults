31ce4a1801204b6f3c41a707c462877b0f039509
stualyttle <stualyttle@users.noreply.github.com>
2025-04-15 21:53:22 +0200

    Update so it works inside project

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,48 +1,35 @@
-import {Config} from 'payload';
-import * as path from 'path';
-import {CONSTANTS, packagePath, setConstants} from './index';
-import {Constants} from './constrant-types';
-
-function get(p: string): string {
-    console.log('get', p);
-    console.log('resolve', path.resolve(packagePath, p));
-    return path.join(packagePath, p);
-}
+import { Config } from 'payload'
 
 // remove secret & db from the config
 type DefaultConfig = Omit<Config, 'db' | 'secret'>;
 
-
-export function getDefaultConfig(constants: Constants): DefaultConfig {
-    setConstants(constants);
-    return {
-        // Default values for the Payload config
-        admin: {
-            components: {
-                Nav: get('components/Navigation'),
-            },
-            livePreview: {
-                breakpoints: [
-                    {
-                        label: 'Mobile',
-                        name: 'mobile',
-                        width: 375,
-                        height: 667,
-                    },
-                    {
-                        label: 'Tablet',
-                        name: 'tablet',
-                        width: 768,
-                        height: 1024,
-                    },
-                    {
-                        label: 'Desktop',
-                        name: 'desktop',
-                        width: 1440,
-                        height: 900,
-                    },
-                ],
-            },
+export const defaultConfig: DefaultConfig = {
+  // Default values for the Payload config
+  admin: {
+    components: {
+      Nav: '@/../libs/payload-defaults/src/components/Navigation',
+    },
+    livePreview: {
+      breakpoints: [
+        {
+          label: 'Mobile',
+          name: 'mobile',
+          width: 375,
+          height: 667,
         },
-    };
-};
+        {
+          label: 'Tablet',
+          name: 'tablet',
+          width: 768,
+          height: 1024,
+        },
+        {
+          label: 'Desktop',
+          name: 'desktop',
+          width: 1440,
+          height: 900,
+        },
+      ],
+    },
+  },
+}
